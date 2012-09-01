552ac9977ca181f93a73c6a6a7763038edbd5527
jmslbam <jmslbam@gmail.com>
2012-09-01 23:14:40 +0200

    New initial commit ^

---
+++ wk3/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wk3::Application.config.secret_token = '90745952335f87d338abe7bc69e9094f3fe91c64c5819ecbfd5f869f607fa3b5574214ce88e60b49b20ded79f356fb2e38716c8c5937c3b44adb69b9baea3b91'
