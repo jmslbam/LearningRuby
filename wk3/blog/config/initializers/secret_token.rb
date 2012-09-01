4fef7b961d58d857c8d29cfbeedf76a08a24584b
jmslbam <jmslbam@gmail.com>
2012-09-01 22:54:07 +0200

    Make room for a new install

--- wk3/blog/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Blog::Application.config.secret_token = 'c3c634b81bd7504b0a2b95b144b6bbed14fcf16fe444d629f04d7bb0a2ecccf15cf764325353a9cfc127bfb5c2c805703e6990a91f0842017714e9ec945a1d92'
--- wk3/blog/test/functional/home_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class HomeControllerTest < ActionController::TestCase
-  test "should get indexe" do
-    get :indexe
-    assert_response :success
-  end
-
-end
