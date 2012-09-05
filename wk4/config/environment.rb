681fe78d976c84be48938dd3c7229380d0494f3a
jmslbam <jmslbam@gmail.com>
2012-09-05 22:34:23 +0200

    Initial commit

---
+++ wk4/config/environment.rb
@@ -0,0 +1,5 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+Wk4::Application.initialize!
---
+++ wk4/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wk4::Application.config.secret_token = '3e20aafa034e4fd52ba1eefd33123a100b5acb3512e37dd27299a4f0dee8ae3e8744b7989eddd41bc88eaf0fe37dba35c948870db0469c22f57a499f239a1cfa'
