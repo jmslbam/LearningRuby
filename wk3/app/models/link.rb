6b4a92a7b9b01604db78038b85571bc562de0f19
Jaime Martinez <jmslbam@gmail.com>
2012-09-07 16:39:59 +0300

    Update wk3/app/models/link.rb
    Moved retreiving maxdesign links by Nokogiri to Link model.

--- wk3/app/models/link.rb
+++ wk3/app/models/link.rb
@@ -1,13 +1,16 @@
 class Link < ActiveRecord::Base
   attr_accessible :name, :title, :url
 
-=begin
-  #wrong number of arguments (1 for 3) 
-  attr_reader :name, :title, :url
-
-  def initialize(name, title , url)
-    @name, @title, @url = name, title, url
-  end 
-=end
+  def self.getExternalLinks
+  
+    items = Array.new
+    
+    doc = Nokogiri::HTML(open('http://www.maxdesign.com.au/category/news/', "User-Agent" => 'Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:15.0) Gecko/20100101 Firefox/15.0'))
+    
+    doc.css('p.entry a').each_with_index do |item, idx|
+      items << Link.new(:name => item.text, :title => item.text, :url => item.at_css("@href").value)
+    end
+    
+  end
 
 end
