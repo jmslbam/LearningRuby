7125b24c07a1a605cd6c4c5124935ea931d6882c
Jaime Martinez <jmslbam@gmail.com>
2012-09-07 16:41:11 +0300

    Update wk3/app/controllers/home_controller.rb
    Moved getting externals links to Link model.

--- wk3/app/controllers/home_controller.rb
+++ wk3/app/controllers/home_controller.rb
@@ -7,22 +7,8 @@ class HomeController < ApplicationController
   def index
   	
     #All db links
-    @links = Link.all
+    @links = Link.all + Link.getExternalLinks
     
-    items = Array.new
-    
-    doc = Nokogiri::HTML(open('http://www.maxdesign.com.au/category/news/', "User-Agent" => 'Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:15.0) Gecko/20100101 Firefox/15.0'))
-    
-    doc.css('p.entry a').each_with_index do |item, idx|
-      items << Link.new(:name => item.text, :title => item.text, :url => item.at_css("@href").value)
-    end
-
-    #items = Scraper.new('http://www.maxdesign.com.au/category/news/').collect
-    # undefined method `stringify_keys' for "Some links for light reading (30/8/12)":String
-    
-    #Merge the db with the scraper
-    @links += items
-
     if @links
       @widget_title = 'Some links for light reading'
     else
