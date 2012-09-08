49030d4263d0e1b279a787439b8c4d904afcaece
jmslbam <jmslbam@gmail.com>
2012-09-08 12:50:44 +0200

    self.new and returning the items

--- wk3/app/models/link.rb
+++ wk3/app/models/link.rb
@@ -11,8 +11,10 @@ class Link < ActiveRecord::Base
     doc = Nokogiri::HTML(open('http://www.maxdesign.com.au/category/news/', "User-Agent" => 'Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:15.0) Gecko/20100101 Firefox/15.0'))
     
     doc.css('p.entry a').each_with_index do |item, idx|
-      items << Link.new(:name => item.text, :title => item.text, :url => item.at_css("@href").value)
+      items << self.new(:name => item.text, :title => item.text, :url => item.at_css("@href").value)
     end
+
+    return items
     
   end
 
